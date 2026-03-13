80973cdc7a2da216db2c3082fdfc76791e65b3df
RexInSen <amatra.sen@gmail.com>
2026-03-13 19:53:10 +0530

    Clean the repository and prepare for v1.1

--- _hook_streamlit.py
+++
@@ -1,6 +0,0 @@
-
-import sys, os
-# Tell Streamlit where its static assets live inside the frozen bundle
-os.environ.setdefault('STREAMLIT_STATIC_FOLDER',
-    os.path.join(getattr(sys, '_MEIPASS', os.path.dirname(sys.executable)),
-                 'streamlit', 'static'))
